a0965437da9fae166fbcc3c3fd5a0372825e51ff
Jonathan Guallasamín <guallasamin@gmail.com>
2023-11-07 11:53:37 -0500

    Initial commit

---
+++ R/kill_connections.R
@@ -0,0 +1,7 @@
+killDbConnections <- function () {
+  all_cons <- dbListConnections(PostgreSQL())
+  print(all_cons)
+  for(con in all_cons)
+    +  dbDisconnect(con)
+  print(paste(length(all_cons), " connections killed."))
+}
